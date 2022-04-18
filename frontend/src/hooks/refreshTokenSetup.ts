6d61e135bc207d173f966ac2f7791f151c2cef37
Jean-Baptiste Castillo <jeanbaptiste.castillo@isolutions.ch>
2022-04-18 19:25:31 +0200

    Created function to refresh token

---
+++ frontend/src/hooks/refreshTokenSetup.ts
@@ -0,0 +1,20 @@
+const refreshTokenSetup = (res: any) => {
+  // Timing to renew access token
+  let refreshTiming = (res.tokenObj.expires_in || 3600 - 5 * 60) * 1000;
+
+  const refreshToken = async () => {
+    const newAuthRes = await res.reloadAuthResponse();
+    refreshTiming = (newAuthRes.expires_in || 3600 - 5 * 60) * 1000;
+    console.log("newAuthRes:", newAuthRes);
+    // saveUserToken(newAuthRes.access_token); <-- to save new token
+    console.log("new auth Token", newAuthRes.id_token);
+
+    // setup the other timer after the first one
+    setTimeout(refreshToken, refreshTiming);
+  };
+
+  // Setup first refresh timer
+  setTimeout(refreshToken, refreshTiming);
+};
+
+export default refreshTokenSetup;
