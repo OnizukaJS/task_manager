406f3640c2a7856c7499585af5cbb4e5062be9cc
Jean-Baptiste Castillo <jeanbaptiste.castillo@isolutions.ch>
2022-04-28 13:00:29 +0200

    Added one comment

--- backend/TaskManager/Dtos/blob/UploadContentRequest.cs
+++ backend/TaskManager/Dtos/blob/UploadContentRequest.cs
@@ -3,6 +3,6 @@
     public class UploadContentRequest
     {
         public string Content { get; set; } = null!;
-        public string FileName { get; set; } = null!;
+        public string FileName { get; set; } = null!; // What I want it to be named in Blob storage
     }
 }
