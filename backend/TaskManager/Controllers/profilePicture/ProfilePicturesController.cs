87596582694ffc80d643021c817300aaecec28f0
Jean-Baptiste Castillo <jeanbaptiste.castillo@isolutions.ch>
2022-05-11 09:43:59 +0200

    Revert commit to where profile picture blob is working fine!

--- backend/TaskManager/Controllers/profilePicture/ProfilePicturesController.cs
+++ backend/TaskManager/Controllers/profilePicture/ProfilePicturesController.cs
@@ -6,6 +6,7 @@ using TaskManager.Models.profilePicture;
 using Azure.Storage.Blobs;
 using TaskManager.Interfaces.employee;
 using TaskManager.Models.taskToDo;
+using Azure.Storage.Sas;
 
 namespace TaskManager.Controllers.profilePicture
 {
@@ -51,6 +52,7 @@ namespace TaskManager.Controllers.profilePicture
                 try
                 {
                     BlobClient blob = container.GetBlobClient(filename);
+                    
                     using (Stream stream = profilePicture.FormFile.OpenReadStream())
                     {
                         blob.Upload(stream);
@@ -80,6 +82,45 @@ namespace TaskManager.Controllers.profilePicture
             blobClient.Delete();
             _employeeQueries.DeleteEmployeeProfilePicture(employeeId);
             _taskToDoContext.SaveChanges();
+        }
+
+        private Uri GetServiceSasUriForBlob(BlobClient blobClient, string storedPolicyName = null)
+        {
+            // Check whether this BlobClient object has been authorized with Shared Key.
+            if (blobClient.CanGenerateSasUri)
+            {
+                // Create a SAS token that's valid for one hour.
+                BlobSasBuilder sasBuilder = new BlobSasBuilder()
+                {
+                    BlobContainerName = blobClient.BlobContainerName,
+                    BlobName = blobClient.Name,
+                    Resource = "b"
+                };
+
+                if (storedPolicyName == null)
+                {
+                    sasBuilder.ExpiresOn = DateTimeOffset.UtcNow.AddHours(1);
+                    sasBuilder.SetPermissions(BlobSasPermissions.Read |
+                        BlobSasPermissions.Write);
+                }
+                else
+                {
+                    sasBuilder.Identifier = storedPolicyName;
+                }
+
+                Uri sasUri = blobClient.GenerateSasUri(sasBuilder);
+                Console.WriteLine("SAS URI for blob is: {0}", sasUri);
+                Console.WriteLine();
+
+                return sasUri;
+            }
+
+            else
+            {
+                Console.WriteLine(@"BlobClient must be authorized with Shared Key 
+                          credentials to create a service SAS.");
+                return null;
+            }
         }
     }
 }
